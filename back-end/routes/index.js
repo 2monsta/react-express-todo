21425e47fa807fc5fc856a394346d21771748645
Jason Li <Yunf.li37@gmail.com>
2017-12-07 10:32:36 -0500

    connect front and back ends

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/getStudents', function(req, res, next) {
+  const students = [
+    "eddie",
+    "valerie",
+    "michael",
+    "scott"
+  ];
+
+  res.json(students);
 });
 
 module.exports = router;
