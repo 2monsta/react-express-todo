261cbd5124f4b0414246314fe4a4453e8f90166f
Jason Li <Yunf.li37@gmail.com>
2017-12-07 13:41:27 -0500

    mysql query added

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,16 +1,67 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require ("mysql");
+var config = require("../config/config");
+
+var connection = mysql.createConnection(config.db);
+
+connection.connect();
 
 /* GET home page. */
 router.get('/getStudents', function(req, res, next) {
-  const students = [
-    "eddie",
-    "valerie",
-    "michael",
-    "scott"
-  ];
 
-  res.json(students);
+  const SELECT_QUERY = "select * from students;";
+  const getStudents = new Promise((resolve, reject)=>{
+    connection.query(SELECT_QUERY, (error, results)=>{
+      if(error){
+        reject(error);
+      }else{
+        resolve(results);
+      }
+    });
+  });
+
+  getStudents
+    .then((results)=>{
+      res.json(results)
+  })
+});
+
+
+router.post("/addStudent", (req, res,next)=>{
+  const STUDENT_NAME = req.body.studentName;
+  const INSERT_QUERY = 'insert into students (name) values (?);';
+  let insertName = new Promise((resolve, reject)=>{
+    connection.query(INSERT_QUERY,[STUDENT_NAME], (error, results)=>{
+      if(error){
+        reject(error);
+      }else{
+        resolve({msg:"success"});
+      }
+    })
+  });
+  let getData = new Promise((resolve, reject)=>{
+    const QUERY = "select * from students;";
+    connection.query(QUERY, (error, results)=>{
+      if(error){
+        reject(error);
+      }else{
+        resolve(results);
+      }
+    })
+  });
+
+  insertName
+  .then((data)=>{
+    return getData;
+  })
+  .then((data)=>{
+    console.log(data);
+    res.json(data);
+  })
+  .catch((error)=>{
+    console.log(error);
+  });
 });
 
 module.exports = router;
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,24 +4,63 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state= {
+      students:[]
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   componentDidMount(){
     axios.get('http://localhost:3000/getStudents')
-      .then((response)=>{
-        console.log(response);
+    .then((response)=>{
+      console.log(response);
+      this.setState({
+        students: response.data
       })
+    })
   }
 
+  // we want to send student name to the server, we cannot use get. WE NEED TO USE POST
+  // THERES is axios .post and there is axios ({})
+  // AXIOS always return a promise, so we will send data to the express server, and wait for a json response when we get it,
+  // we will move forward
+  handleSubmit(e){
+    e.preventDefault();
+    const STUDENT_NAME = document.getElementById("new-student").value;
+    axios({
+      method: "POST",
+      url: "http://localhost:3000/addStudent",
+      data: {
+        studentName: STUDENT_NAME
+      }
+    })
+    .then((data)=>{
+      console.log(data);
+      this.setState({
+        students:data.data
+      });
+    });
+  }
   render() {
+    let studentsArray = this.state.students.map((student, index)=>(
+        <li key={index}>{student.name}</li>
+      )
+    );
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" id={"new-student"} placeholder={"New Student Name"}/>
+          <button type={"submit"}>Add Student</button>
+        </form>
+        <ul>
+          {studentsArray}
+        </ul>
       </div>
     );
   }
